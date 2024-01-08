4b157c690a0c1261f8d1793654ed510b70161542
brtkn <haha>
2024-01-08 20:30:43 +0300

    Update issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -42,7 +42,9 @@ const IssueForm = ({ issue }: Props) => {
   const onSubmit = handleSubmit(async (data) => {
     try {
       setSubmitting(true);
-      await axios.post("/api/issues", data), router.push("/issues");
+      if (issue) await axios.patch("/api/issues/" + issue.id, data);
+      else await axios.post("/api/issues", data);
+      router.push("/issues");
     } catch (error) {
       setSubmitting(false);
       setError("An unexpected error occurred");
@@ -75,7 +77,8 @@ const IssueForm = ({ issue }: Props) => {
         />
         {<ErrorMessage>{errors.description?.message}</ErrorMessage>}
         <Button disabled={isSubmitting}>
-          Submit New Issue {isSubmitting && <Spinner />}
+          {issue ? "Update Issue" : "Submit New Issue"}{" "}
+          {isSubmitting && <Spinner />}
         </Button>
       </form>
     </div>
