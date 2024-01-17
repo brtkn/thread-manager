76e610600020b716b65789b0b30cf9c957896ac3
brtkn <haha>
2024-01-17 23:55:00 +0300

    add a drop-down menu to show the current user

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -4,7 +4,14 @@ import { usePathname } from "next/navigation";
 import { FaBugSlash } from "react-icons/fa6";
 import classNames from "classnames";
 import { useSession } from "next-auth/react";
-import { Box, Container, Flex } from "@radix-ui/themes";
+import {
+  Avatar,
+  Box,
+  Container,
+  DropdownMenu,
+  Flex,
+  Text,
+} from "@radix-ui/themes";
 const links = [
   { label: "Dashboard", href: "/" },
   { label: "Issues Page", href: "/issues/list" },
@@ -42,8 +49,27 @@ const Navbar = () => {
           </Flex>
           <Box>
             {status === "authenticated" && (
-              <Link href="/api/auth/signout">Logout </Link>
+              <DropdownMenu.Root>
+                <DropdownMenu.Trigger>
+                  <Avatar
+                    src={session.user!.image!}
+                    fallback="?"
+                    size="2"
+                    radius="full"
+                    className="cursor-pointer"
+                  />
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Content>
+                  <DropdownMenu.Label>
+                    <Text size="2">{session.user!.email}</Text>
+                  </DropdownMenu.Label>
+                  <DropdownMenu.Item>
+                    <Link href="/api/auth/signout">Logout </Link>
+                  </DropdownMenu.Item>
+                </DropdownMenu.Content>
+              </DropdownMenu.Root>
             )}
+
             {status === "unauthenticated" && (
               <Link href="/api/auth/signin">Login</Link>
             )}
