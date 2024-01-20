d8038c9eed77db3ff178c14c588af63a46302ee5
brtkn <haha>
2024-01-20 10:42:57 +0300

    enhance the API to assign status

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,8 +1,10 @@
 import authOptions from "@/app/auth/authOptions";
-import { IssueSchema } from "@/app/validationSchema";
+import { issueSchema } from "@/app/validationSchema";
 import prisma from "@/prisma/client";
+import { error } from "console";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
+import { patchIssueSchema } from "@/app/validationSchema";
 
 export async function PATCH(
   request: NextRequest,
@@ -13,10 +15,21 @@ export async function PATCH(
 
   const body = await request.json();
 
-  const validation = IssueSchema.safeParse(body);
+  const validation = patchIssueSchema.safeParse(body);
 
   if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 });
+
+  //  const { assignedToUserId, title, description } = body;
+  if (body.assignedToUserId) {
+    const user = await prisma.user.findUnique({
+      where: { id: body.assignedToUserId },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: "Invalid user" }, { status: 400 });
+    }
+  }
 
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
@@ -24,9 +37,14 @@ export async function PATCH(
 
   if (!issue) return NextResponse.json("Invalid Issue", { status: 404 });
 
+  // You can simplfy your code with destructuring const {assignedToUser,title,description} = body; then title: title > you can write just title.
   const updatedIssue = await prisma.issue.update({
     where: { id: issue.id },
-    data: { title: body.title, description: body.description },
+    data: {
+      title: body.title,
+      description: body.description,
+      assignedToUserId: body.assignedToUserId,
+    },
   });
 
   return NextResponse.json(updatedIssue, { status: 200 });
