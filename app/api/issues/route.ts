ecbbbaf18d60c67b85677615913b43d7df5d01b1
brtkn <haha>
2024-01-21 11:25:51 +0300

    sort issues

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
-import { IssueSchema } from "../../validationSchema";
+import { issueSchema } from "../../validationSchema";
 import { getServerSession } from "next-auth";
 import authOptions from "@/app/auth/authOptions";
 
@@ -10,7 +10,7 @@ export async function POST(request: NextRequest) {
 
   const body = await request.json();
 
-  const validation = IssueSchema.safeParse(body);
+  const validation = issueSchema.safeParse(body);
 
   if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 });
--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -32,8 +32,15 @@ const IssuesPage = async ({ searchParams }: Props) => {
     ? searchParams.status
     : undefined;
 
+  const orderBy = columns.map((col) => col.value).includes(searchParams.orderBy)
+    ? {
+        [searchParams.orderBy]: "asc",
+      }
+    : undefined;
+
   const issues = await prisma.issue.findMany({
     where: { status: status },
+    orderBy: orderBy,
   });
   return (
     <div>
