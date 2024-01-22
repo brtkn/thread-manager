8126dfc8bacdbcf299fdecf9bde3be84c0d780a9
brtkn <haha>
2024-01-22 13:54:25 +0300

    build the IssueSummary component

---
+++ app/IssueSummary.tsx
@@ -0,0 +1,43 @@
+import { Status } from "@prisma/client";
+import { Card, Flex, Text } from "@radix-ui/themes";
+import Link from "next/link";
+
+interface Props {
+  open: number;
+  inProgress: number;
+  closed: number;
+}
+
+const IssueSummary = ({ open, inProgress, closed }: Props) => {
+  const containers: { label: string; value: number; status: Status }[] = [
+    {
+      label: "Open Issues",
+      value: open,
+      status: "OPEN",
+    },
+    { label: "Closed Issues", value: closed, status: "CLOSED" },
+    { label: "In Progress", value: inProgress, status: "IN_PROGRESS" },
+  ];
+
+  return (
+    <Flex gap="4">
+      {containers.map((container) => (
+        <Card key={container.label}>
+          <Flex direction="column" gap="1">
+            <Link
+              href={`/issues/list?status=${container.status}`}
+              className="text-sm font-medium"
+            >
+              {container.label}
+            </Link>
+            <Text size="5" className="font-bold">
+              {container.value}
+            </Text>
+          </Flex>
+        </Card>
+      ))}
+    </Flex>
+  );
+};
+
+export default IssueSummary;
